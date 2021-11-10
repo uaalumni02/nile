8ead75a3864d601736cdf7a55ab0cfc4188cef43
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-11-10 13:47:37 -0500

    run book migration

--- db/migrate/20211110184634_create_books.rb
+++ db/migrate/20211110184634_create_books.rb
@@ -2,9 +2,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :string
       t.string :author
-      t.string :string
 
       t.timestamps
     end
