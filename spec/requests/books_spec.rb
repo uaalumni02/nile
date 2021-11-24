65feff9315e3ed7cde69b3330be5d7044086ef5d
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-11-24 15:36:09 -0500

    add post spec

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,13 +2,24 @@ require 'rails_helper'
 
 
 describe 'Books API', type: :request do
+  describe 'GET /books' do
     it ' returns all books' do
         FactoryBot.create(:book, title: '1984', author: 'George Orwell')
         FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G. Wells')
 
-      get '/books'
+      get '/api/v1/books'
 
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body).size).to eq(2)
     end
+end
+
+  describe 'POST /books' do
+    it 'creates a new book' do
+
+      post '/api/v1/books', params: { book: {title: 'The Martian', author: 'Andy Weir'} }
+
+      expect(response).to have_http_status(:created)
+    end
+  end
 end
