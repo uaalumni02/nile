b35ad599bf091130c7f17813c04478ece19e9f7d
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-11-24 15:49:20 -0500

    :add delete spec

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -16,10 +16,20 @@ end
 
   describe 'POST /books' do
     it 'creates a new book' do
-
-      post '/api/v1/books', params: { book: {title: 'The Martian', author: 'Andy Weir'} }
+     expect {
+       post '/api/v1/books', params: { book: {title: 'The Martian', author: 'Andy Weir'} }
+     }.to change { Book.count }.from(0).to(1)
 
       expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'DELETE /books/:id' do
+    it 'deletes a book' do
+      FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G. Wells')
+      delete '/api/v1/books/1'
+
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
