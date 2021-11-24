57e106bc79a452c870254cb72d61ab69a15a2bbc
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-11-24 15:11:31 -0500

    add versioning

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,3 +1,6 @@
+module Api
+  module V1
+
 class BooksController < ApplicationController
 
   def index
@@ -24,6 +27,7 @@ class BooksController < ApplicationController
   
   def book_params
     params.require(:book).permit(:title, :author)
+      end
+    end
   end
-
 end
