e160ee728e6cee44993458a12b571f3a18341b1d
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-12-10 15:21:26 -0500

    update controller and spec

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -9,7 +9,8 @@ class BooksController < ApplicationController
   end
 
   def create
-   book = Book.new(book_params)
+    author = Author.create!(author_params)
+   book = Book.new(book_params.merge(author_id: author.id))
 
     if book.save
       render json: book, status: :created
@@ -25,9 +26,13 @@ class BooksController < ApplicationController
   end
 
   private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :age)
+  end
   
   def book_params
-    params.require(:book).permit(:title, :author)
+    params.require(:book).permit(:title)
       end
     end
   end
